1d40ac9dd9835bea5ebd71e8125fc3c29718f756
Elburguer <142627932+Elburguer@users.noreply.github.com>
2023-10-13 00:22:26 -0600

    Create ELM_c.m

---
+++ ELM_c.m
@@ -0,0 +1,10 @@
+%Parametros
+Ra=2; %Resistencia 1 (Ohms)
+La=0.023; %Inductor (Faradios) 
+b=0.0012; %Amortiguador
+Kt=0.01; %Motor
+Ke=0.01; %Motor
+J=0.001; %Inercia
+
+open("electrico_mecanico.slx")
+open("Electrico_mecanico_simscape.slx")
